939d9ffaa88582c89e7e39b74ea088f54974559e
boneless-3635 <phamminhhoang317@gmail.com>
2023-01-02 19:38:53 +0700

    cleaned up product validation code

--- Product.java
+++ Product.java
@@ -17,10 +17,10 @@ import java.util.UUID;
 import java.util.regex.Pattern;
 
 public class Product {
-    private String id;
+    private final String id;
     private final String NAME;
     private int price;
-    private String category;
+    private final String category;
 
     public Product(String id, String NAME, int price, String category) {
         this.id = id;
@@ -31,6 +31,7 @@ public class Product {
 
     public static void writeProduct() throws IOException {
         while (true) {
+            boolean errorFree = true;
             System.out.println("Create new product: ");
             String errorMessage = "Errors: \n";
             Scanner userInput = new Scanner(System.in);
@@ -43,43 +44,58 @@ public class Product {
             String category = userInput.nextLine();
 
 //            input validating
-            if (!validateInput(productName, "^[a-zA-Z0-9 ]{3,}$")) {
+            if (validateInput(productName, "^[a-zA-Z0-9 ]{3,}$")) {
                 errorMessage += "Invalid user name (only letters and digits, at least 3 characters) \n";
+                errorFree = false;
+            }
 
-                if (!validateInput(String.valueOf(price), "^[0-9]{4,}$")) {
-                    errorMessage += "Invalid price (must be at least 1000 VND) \n";
-                }
+            if (validateInput(String.valueOf(price), "^[0-9]{4,}$")) {
+                errorMessage += "Invalid price (must be at least 1000 VND) \n";
+                errorFree = false;
+            }
 
-                if (!validateInput(category, "^.+@.+$")) {
-                    errorMessage += "Invalid email (must contains @ in email) \n";
-                }
-
-
-                if (errorMessage != "Errors: \n") {
-                    System.out.println(errorMessage);
-                } else {
+            if (validateInput(category, "^.+@.+$")) {
+                errorMessage += "Invalid email (must contains @ in email) \n";
+                errorFree = false;
+            }
+            
+            if (!errorFree) {
+                System.out.println(errorMessage);
+            } else {
 //                    generate product id
-                    String productID = UUID.randomUUID().toString();
+                String productID = UUID.randomUUID().toString();
 //                    write info to file
-                    PrintWriter pw = new PrintWriter(new FileWriter("product.txt", true));
-                    pw.println(productID + ";" + productName + ";" + price + ";" + category);
-                    pw.close();
-                    System.out.println("Register successfully!");
-                    break;
-                }
+                PrintWriter pw = new PrintWriter(new FileWriter("product.txt", true));
+                pw.println(productID + ";" + productName + ";" + price + ";" + category);
+                pw.close();
+                System.out.println("Register successfully!");
+                break;
+            }
             }
         }
-    }
 
     public static boolean validateInput(String userInput, String pattern) {
-        boolean valid;
         Pattern validPattern = Pattern.compile(pattern);
-        if (validPattern.matcher(userInput).find()) {
-            valid = true;
-        } else {
-            valid = false;
-        }
-        return valid;
+        return !validPattern.matcher(userInput).find();
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
 }
