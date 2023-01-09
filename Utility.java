a7f4a86acbf32dad62a3fefb0ea0f92161fe300e
boneless-3635 <phamminhhoang317@gmail.com>
2023-01-09 16:26:35 +0700

    Added another parameter to updateTextFile
    To support both name and id for this method.

--- Utility.java
+++ Utility.java
@@ -7,7 +7,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Utility {
-    public static void updateTextFile(String productName, String updateValue, int indexNeedToUpdate, String fileName) throws IOException {
+    public static void updateTextFile(String nameOrID, String updateValue, int indexSource, int indexNeedToUpdate,
+                                      String fileName) throws IOException {
         //temporary arraylist to store lines
         ArrayList<String> tempLines = new ArrayList<String>();
         Scanner fileScanner = new Scanner(Paths.get(fileName));
@@ -15,7 +16,7 @@ public class Utility {
             String line = fileScanner.nextLine();
             String [] lineArray = line.split(",");
             //split a line to array, then use index 0 storing id to take out the product need to be updated
-            if (lineArray[1].equals(productName)){
+            if (lineArray[indexSource].equals(nameOrID)){
                 //after update the index with given value, turn in to string again
                 lineArray[indexNeedToUpdate] = String.valueOf(updateValue);
                 line = String.join(",", lineArray);
